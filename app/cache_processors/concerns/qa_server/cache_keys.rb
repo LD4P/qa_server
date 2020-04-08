78f45aed804fb6cbbc06bf4d74016555ac9187bb
E. Lynette Rayle <elr37@cornell.edu>
2020-04-08 14:32:11 -0400

    move performance day graph generation to background job
    Also removes data cache for day and month graph.  See note about data caching for performance graphs in PR #330 which moved year graph to background job.
    
    See also, #331 which moves

--- app/cache_processors/concerns/qa_server/cache_keys.rb
+++ app/cache_processors/concerns/qa_server/cache_keys.rb
@@ -7,6 +7,5 @@ module QaServer
     SCENARIO_RUN_HISTORY_DATA_CACHE_KEY = "QaServer--CacheKeys--scenario_run_history_data"
 
     PERFORMANCE_DATATABLE_DATA_CACHE_KEY = "QaServer--Cache--performance_datatable_data"
-    PERFORMANCE_GRAPH_DAY_DATA_CACHE_KEY = "QaServer--CacheKeys--performance_graph_day_data"
   end
 end
--- app/cache_processors/qa_server/performance_day_graph_cache.rb
+++ app/cache_processors/qa_server/performance_day_graph_cache.rb
@@ -1,64 +1,26 @@
 # frozen_string_literal: true
-# Generate graphs for the past 24 hours using cached data.  The last hour of data is always calculated and all graphs
-# for are generated.
+# Cache the datetime_stamp of the last time the performance day graph was generated.  Calls job to generate the graph if expired.
 module QaServer
   class PerformanceDayGraphCache
-    class_attribute :authority_list_class, :graph_data_service, :graphing_service
-    self.authority_list_class = QaServer::AuthorityListerService
-    self.graph_data_service = QaServer::PerformanceGraphDataService
-    self.graphing_service = QaServer::PerformanceGraphingService
-
     class << self
-      include QaServer::CacheKeys
-      include QaServer::PerformanceHistoryDataKeys
-
       # Generates graphs for the past 24 hours for :search, :fetch, and :all actions for each authority.
       # @param force [Boolean] if true, run the tests even if the cache hasn't expired; otherwise, use cache if not expired
       def generate_graphs(force: false)
-        QaServer.config.monitor_logger.debug("(QaServer::PerformanceDayGraphCache) - GENERATING performance day graphs (force: #{force})")
-        QaServer.config.performance_cache.write_all
-        generate_graphs_for_authorities(force: force)
+        Rails.cache.fetch(cache_key, expires_in: next_expiry, race_condition_ttl: 30.seconds, force: force) do
+          QaServer.config.monitor_logger.debug("(QaServer::PerformanceDayGraphCache) - KICKING OFF PERFORMANCE DAY GRAPH GENERATION (force: #{force})")
+          QaServer::PerformanceDayGraphJob.perform_later
+          "Graphs generation initiated at #{QaServer::TimeService.current_time}"
+        end
       end
 
       private
 
-        def generate_graphs_for_authorities(force:)
-          auths = authority_list_class.authorities_list
-          generate_graphs_for_authority(authority_name: ALL_AUTH, force: force) # generates graph for all authorities
-          auths.each { |authname| generate_graphs_for_authority(authority_name: authname, force: force) }
+        def cache_key
+          "QaServer::PerformanceDayGraphCache.generate_graphs--latest_generation_initiated"
         end
 
-        def generate_graphs_for_authority(authority_name:, force:)
-          [SEARCH, FETCH, ALL_ACTIONS].each_with_object({}) do |action, hash|
-            hash[action] = generate_24_hour_graph(authority_name: authority_name, action: action, force: force)
-          end
-        end
-
-        def generate_24_hour_graph(authority_name:, action:, force:)
-          graph_created = false
-          data = Rails.cache.fetch(cache_key_for_authority_action(authority_name: authority_name, action: action),
-                                   expires_in: QaServer::TimeService.current_time.end_of_hour - QaServer::TimeService.current_time,
-                                   race_condition_ttl: 1.hour, force: force) do
-            data = graph_data_service.calculate_last_24_hours(authority_name: authority_name, action: action)
-            graphing_service.generate_day_graph(authority_name: authority_name, action: action, data: data)
-            graph_created = true
-            data
-          end
-          regen_last_hour_and_graph(authority_name: authority_name, action: action, data: data) unless graph_created
-        end
-
-        def regen_last_hour_and_graph(authority_name:, action:, data:)
-          Rails.cache.fetch(cache_key_for_authority_action(authority_name: authority_name, action: action),
-                            expires_in: QaServer::TimeService.current_time.end_of_hour - QaServer::TimeService.current_time,
-                            race_condition_ttl: 1.hour, force: true) do
-            data = graph_data_service.recalculate_last_hour(authority_name: authority_name, action: action, averages: data)
-            graphing_service.generate_day_graph(authority_name: authority_name, action: action, data: data)
-            data
-          end
-        end
-
-        def cache_key_for_authority_action(authority_name:, action:)
-          "#{PERFORMANCE_GRAPH_DAY_DATA_CACHE_KEY}--#{authority_name}--#{action}"
+        def next_expiry
+          QaServer::CacheExpiryService.end_of_hour_expiry
         end
     end
   end
